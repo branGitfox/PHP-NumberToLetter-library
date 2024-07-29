7453d6f87a212a292cef49b7fbad5f8abeefb00b
BranGitFox <vixfgit@gmail.com>
2024-07-29 04:11:47 +0200

    fixed some but

--- NumberToLetter/test.php
+++ NumberToLetter/test.php
@@ -3,4 +3,6 @@
 require './NumberToLetter.php';
 
 //use the static method
-NumberToLetter::makeTest(0,10 , 'ENG');
+// NumberToLetter::makeTest(9999,10, 'ENG');
+$ntl = new NumberToLetter(999999999999, 'ENG');
+echo $ntl->numberToLetter();
