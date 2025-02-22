0b339f88c82c866aae22ac502c6984acb3a7467e
BranGitfox <vixfgit@gmail.com>
2025-02-22 13:37:04 +0300

    ref: changing  all img'

--- NumberToLetter/test.php
+++ NumberToLetter/test.php
@@ -1,8 +1,8 @@
-<?php 
+<?php
 //import the class from 'NumberToletter/NumberToletter.php'
 require './NumberToLetter.php';
 
 //use the static method
-// NumberToLetter::makeTest(9999,10, 'ENG');
-$ntl = new NumberToLetter(999999999999, 'ENG');
-echo $ntl->numberToLetter();
+NumberToLetter::makeTest(0,10, 'ENG');
+// $ntl = new NumberToLetter(999999999999, 'ENG');
+// echo $ntl->numberToLetter();
--- README.md
+++ README.md
@@ -2,7 +2,7 @@
 A PHP library that allows PHP developer to convert a  `Number` from 0 to 999,999,999,999 to a  `letter String`.
 you can get the documentation [here](https://php-number-to-letter-library.vercel.app/)
 ## Website
-<img src="./assets/img/NTLp.png">
+<img src="./assets/img/code-4.png">
 
 
 
